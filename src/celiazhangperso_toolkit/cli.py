d64a296f476bbb2c8d869501184e1f679b78bacf
Celia ZHANG <celiazhang349@gmail.com>
2025-03-10 15:46:05 +0000

    try with pre-commit

--- src/celiazhangperso_toolkit/cli.py
+++ src/celiazhangperso_toolkit/cli.py
@@ -1,11 +1,12 @@
 import click
 
+
 @click.command()
 @click.option(
     "--name",
     help="The person to greet.",
 )
-def hello(name):
+def hello(name: str) -> None:
     """Say hello to NAME."""
     click.echo(f"Hello {name}!")
 
